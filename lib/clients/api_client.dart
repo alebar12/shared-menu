442e2e81443761b4898ff8c427c94095fe5d68f5
alebar12 <abarducci@proton.me>
2026-09-06 14:47:48 +0200

    refactor: moved api main url into constant file

--- lib/clients/api_client.dart
+++ lib/clients/api_client.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
+import 'package:shared_menu/constants/consts.dart';
 import 'package:shared_menu/dto/meal.dart';
 import 'package:shared_menu/services/storage_service.dart';
 
@@ -10,7 +11,7 @@ class ApiClient {
 
   Future<String> fetchMenuId() async {
     final response = await http
-        .get(Uri.parse('https://shared-menu.alebar12.workers.dev/menuId'));
+        .get(Uri.parse('${Consts.apiBaseUrl}/menuId'));
     if (response.statusCode != 200) {
       throw ApiException(response.statusCode, 'Failed to fetch menu id');
     }
@@ -20,7 +21,7 @@ class ApiClient {
   Future<List<Meal>> fetchMeals() async {
     String menuId = await StorageService().getMenuId();
     final response = await http
-        .get(Uri.parse('https://shared-menu.alebar12.workers.dev/meals'),
+        .get(Uri.parse('${Consts.apiBaseUrl}/meals'),
         headers: <String, String>{
           'x-menu-id': menuId,
         },
@@ -33,7 +34,7 @@ class ApiClient {
 
   Future<http.Response> updateMeal(String meal, DateTime day, MealType mealType, String menuId) async {
     final response = await http.post(
-      Uri.parse('https://shared-menu.alebar12.workers.dev/meals'),
+      Uri.parse('${Consts.apiBaseUrl}/meals'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
         'x-menu-id': menuId,
